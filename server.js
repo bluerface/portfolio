0233034a81224e972d84d61a9d88b6a92acd6141
bluerface <fallingsilkworm@gmail.com>
2017-02-10 13:05:23 +0000

    [FEAT-1] Get markdown from file

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  data = {markdown: marked('# Marked in browser\n\nRendered by **marked**.')}
+  data = {}
+  var path = __dirname + '/data/markdown.md'
+  var markdown = fs.readFileSync(path, 'utf8')
+  data.markdown = marked(markdown);
   res.render('index.ejs', data)
 });
 
